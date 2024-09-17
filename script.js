cb394fdc7285460ff47a5189efbeefc776faa4db
Ember Doyle <doyle.ember@gmail.com>
2024-09-17 09:20:55 -0600

    Fix delete and add book bugs

--- script.js
+++ script.js
@@ -12,15 +12,18 @@ function Book(Title,Author,Pages,ReadStatus){ //constructor
         
 }
 
-function addBooktoLibrary(book){
+function addBooktoLibrary(book){//adds books to array
     return myLibrary.push(book);
 }
 
-function displayLibrary(){
-    //for each book in library add a new div under body
+function displayLibrary(){ //for each book in library array add a new div in HTML
+    let grid = document.querySelector(".gridContainer");
     return myLibrary.forEach(function(book,index){
-        let libraryArrayIndex = index;
-        let grid = document.querySelector(".gridContainer");
+        //if book not already on page
+        let bookTitleNoSpaces = book.Title.replace(/\s/g, '');
+        let bookSearch =document.querySelector("."+bookTitleNoSpaces);
+        if(bookSearch == null){
+            let libraryArrayIndex = index;
         let newBook = document.createElement("div");
         newBook.classList.add("book");
         newBook.id ="book"+libraryArrayIndex;
@@ -34,12 +37,22 @@ function displayLibrary(){
         newBook.appendChild(removeButton);
         //for each oject parameter add a div and print
         for(var info in book){
+            //when info is title - add name as title
             let bookInfo = document.createElement("div");
-            bookInfo.classList.add(info);
+            let bookTitle = book.Title;
+            if(info=="Title"){
+                console.log(bookTitle);
+                //remove spaces
+                let bookTitleNoSpaces = bookTitle.replace(/\s/g, '');
+                bookInfo.classList.add(bookTitleNoSpaces);
+            }
+            else{
+                bookInfo.classList.add(info);
+            }
             bookInfo.id =info+libraryArrayIndex;
             bookInfo.textContent = info + ": " + book[info];
             newBook.appendChild(bookInfo);
-
+            
         }
         //read and not read buttons
         let readButton = document.createElement("button");
@@ -53,23 +66,25 @@ function displayLibrary(){
         notReadButton.id =libraryArrayIndex;
         newBook.appendChild(notReadButton);
         return
-    });
 
+        }
+        return
+        
+    });
 }
 
+//books to start library with
 const theHobbit = new Book("The Hobbit","J.R.R Tolkien", "295", "Not yet read");
 const Cerulean = new Book("The House on the Cerulean Sea","T.J Klune", "394", "Read");
 const Witch = new Book("A Very Secret Society of Irregular Witches","Sangu Mandanna", "318", "Read");
-
+//add these books to library
 addBooktoLibrary(theHobbit);
 addBooktoLibrary(Cerulean);
 addBooktoLibrary(Witch);
-
-console.log("my Library array is");
-console.log(myLibrary);
+//add library to HTML
 displayLibrary();
 
-//useraddedbooks
+//user adding books using form
 const addBookButton = document.querySelector(".newBook");
 const form = document.querySelector("form");
 const submitButton = document.querySelector("#submit");
@@ -88,24 +103,18 @@ submitButton.addEventListener("click",(e)=>{
     const newBook = new Book(userInputtedBookName,userInputtedAuthor,userInputtedPages,userInputtedStatus);
     console.log(newBook);
     addBooktoLibrary(newBook);
-    //delete everything in grid container
-    let bookCards = document.querySelectorAll(".gridContainer>*");
-    let grid = document.querySelector(".gridContainer");
-    bookCards.forEach(function(child){
-        grid.removeChild(child);
-    })
-    //redisplaylib
+    //addnewbooto webpage
     displayLibrary();
   
 });
 
 const closeButton = document.querySelector("#close");
-submitButton.addEventListener("click",(e)=>{
+closeButton.addEventListener("click",(e)=>{
     e.preventDefault();
     form.style.display = "none";//hide form
 });
 
-//removebookfromlibray
+//remove book from libray
 const removeButtons = document.querySelectorAll(".remove");
 const grid = document.querySelector(".gridContainer");
 
@@ -114,10 +123,15 @@ removeButtons.forEach((button)=>{
         const bookToRemove = document.querySelector("#book"+button.id);
         grid.removeChild(bookToRemove);
 
+        //delete from mylibrary
+        let myLibraryEntry = myLibrary[button.id];
+        myLibrary.splice(button.id,button.id+1);
+        console.log(myLibrary);
+
     });
 });
 
-//read button
+//change book status to read
 const readButtons = document.querySelectorAll(".read");
 readButtons.forEach((readButton)=>{
     readButton.addEventListener("click",()=>{
@@ -129,7 +143,7 @@ readButtons.forEach((readButton)=>{
     });
 });
 
-//Notread button
+////change book status to not read
 const notReadButtons = document.querySelectorAll(".notRead");
 notReadButtons.forEach((notReadButton)=>{
     notReadButton.addEventListener("click",()=>{
